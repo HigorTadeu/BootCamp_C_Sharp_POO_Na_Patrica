6ce6c92e4e81e9c0f5dafdd0a00c54410d62cf1c
Higor Tadeu <higorse@gmail.com>
2024-08-27 18:30:07 -0300

    Etapa 02: Herança e Polimorfismo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BootCamp_C_Sharp_POO_Na_Patrica.Models;
+
+Console.WriteLine("Hello, World!");
+
+
+Aluno a1 = new Aluno();
+a1.Nome = "Higor";
+a1.Idade = 40;
+a1.Email = "teste@teste.com";
+a1.Nota = 10;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "José";
+p1.Idade = 50;
+p1.Salario = 1000;
+p1.Apresentar();
